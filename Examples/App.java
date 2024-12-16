c07a22f372e85b3382c2b600f75c0ff7d1ae8147
Berke Sertel <76247074+SweetRomeo@users.noreply.github.com>
2024-12-16 16:40:23 +0300

    Add files via upload

---
+++ Examples/App.java
@@ -0,0 +1,34 @@
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5433/demo";
+        String user = "postgres";
+        String password = "fgJKlmay6n9=)";
+        String sql = "select * from student";
+
+        try {
+            /*
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            rs.next();
+            System.out.println("Connected to PostgreSQL Database");
+            String studentName = rs.getString("sname");
+            System.out.println("The name of the Student is : " + studentName);
+            conn.close();
+            System.out.println("Disconnected from PostgreSQL Database");
+            */
+            Student student = new Student();
+            student.connectDatabase(url, user, password);
+            student.executeQuery(sql);
+            student.printStudentInfo();
+            student.closeConnection();
+        }
+        catch (Exception ex) {
+            System.out.println("Exception caught: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ Examples/Student.java
@@ -0,0 +1,116 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Student {
+    private int id;
+    private int age;
+    private String name;
+    private String gender;
+    Connection connection = null;
+    ResultSet resultSet = null;
+    Statement statement = null;
+    private String addStudentSql;
+    private java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+    public Student() {
+    }
+
+    public Student(int id, int age, String name, String gender) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public void connectDatabase(String url, String user, String pass) {
+        try {
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (SQLException e) {
+            System.out.println("Exception caught: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public void executeQuery(String loadStudentSql) {
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(loadStudentSql);
+            //resultSet.next();
+        } catch (SQLException e) {
+            System.out.println("Exception caught: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() throws SQLException {
+        return resultSet.getString("sname");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void closeConnection() throws SQLException {
+        connection.close();
+        System.out.println("Connection closed");
+    }
+
+    @Override
+    public String toString() {
+        try {
+            return "Student{" +
+                    "id=" + resultSet.getInt(1) +
+                    ", age=" + resultSet.getInt(2) +
+                    ", name='" + resultSet.getString(3) + '\'' +
+                    ", gender='" + resultSet.getString(4) + '\'' +
+                    '}';
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void printStudentInfo() throws SQLException {
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " "
+                    + resultSet.getString(3) + " " + resultSet.getString(4));
+        }
+    }
+
+    public void addStudent() throws SQLException {
+        String studentName = scanner.nextLine();
+        String studentGender = scanner.nextLine();
+        int studentAge = Integer.parseInt(scanner.nextLine());
+        int studentId = Integer.parseInt(scanner.nextLine());
+        String addStudentSql = "insert into student values (" + studentId + ",\'" + studentName + "\',\' " + studentGender + "\', " + studentAge + ")";
+        statement = connection.createStatement();
+        statement.execute(addStudentSql);
+    }
+}
